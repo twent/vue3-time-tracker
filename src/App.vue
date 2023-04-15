664a613e1fd236d67c564faafbda9ebc5accd100
twent <twentprod@gmail.com>
2023-04-15 14:41:36 +0300

    ✅ Add header (navbar)
    - add sticky header (navbar)
    - add @heroicons/vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ClockIcon, CheckCircleIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
-  <HelloWorld msg="You did it!" />
+  <header
+    class="sticky top-0 z-20 flex items-center justify-between border-b bg-white/70 p-4 shadow-sm backdrop-blur"
+  >
+    <a class="font-lg flex items-center gap-2 text-xl font-semibold text-gray-800" href="/">
+      <ClockIcon class="h-7 text-purple-600" />
+      <span>TimeTracker</span>
+    </a>
+
+    <div class="text-sm font-semibold text-gray-600">
+      <div v-if="true" class="flex items-center gap-1 md:gap-1.5">
+        <span>Day complete!</span>
+        <CheckCircleIcon class="h-7 text-green-400" />
+      </div>
+
+      <div v-else class="flex items-center gap-1 md:gap-1.5">
+        <div>Progress: <span class="font-mono">20%</span></div>
+        <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style scoped></style>
