2b57c4104ee0695cc8d50b385262bd90c0483511
twent <twentprod@gmail.com>
2023-04-15 16:04:47 +0300

    ✅ Add bottom navigation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { ClockIcon, CheckCircleIcon } from '@heroicons/vue/24/outline'
+import { ClockIcon, CheckCircleIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
   <header
     class="sticky top-0 z-20 flex items-center justify-between border-b bg-white/70 p-4 shadow-sm backdrop-blur"
   >
-    <a class="font-lg flex items-center gap-2 text-xl font-semibold text-gray-800" href="/">
+    <a class="flex items-center gap-2 text-base font-bold text-gray-700 sm:text-lg" href="/">
       <ClockIcon class="h-7 text-purple-600" />
       <span>TimeTracker</span>
     </a>
@@ -23,6 +23,39 @@ import { ClockIcon, CheckCircleIcon } from '@heroicons/vue/24/outline'
       </div>
     </div>
   </header>
+
+  <main class="flex grow flex-col"></main>
+
+  <nav class="sticky bottom-0 z-20 border-t bg-white/60 px-4 py-2.5 shadow-sm backdrop-blur">
+    <ul
+      class="flex items-center justify-around gap-4 text-center text-xs font-semibold capitalize text-gray-600"
+    >
+      <li class="flex-1">
+        <a
+          class="flex flex-col items-center gap-1 transition hover:text-purple-600"
+          href="#timeline"
+        >
+          <ClockIcon class="w-6" /> Timeline
+        </a>
+      </li>
+      <li class="flex-1">
+        <a
+          class="flex flex-col items-center gap-1 transition hover:text-purple-600"
+          href="#activities"
+        >
+          <ListBulletIcon class="w-6" /> Activities
+        </a>
+      </li>
+      <li class="flex-1">
+        <a
+          class="flex flex-col items-center gap-1 transition hover:text-purple-600"
+          href="#progress"
+        >
+          <ChartBarIcon class="w-6" /> Progress
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style scoped></style>
