de57164aab0f8a0e490a8170b331971b95070eaf
twent <twentprod@gmail.com>
2023-04-17 20:38:03 +0300

    ✅ Add dynamic variable for current page

--- src/components/TheBottomNavigation.vue
+++ src/components/TheBottomNavigation.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import NavItem from './NavItem.vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 
@@ -7,6 +8,8 @@ const navItems = {
   activities: ListBulletIcon,
   progress: ChartBarIcon
 }
+
+const currentPage = ref('timeline')
 </script>
 
 <template>
@@ -14,7 +17,13 @@ const navItems = {
     <ul
       class="flex items-center justify-around gap-4 text-center text-xs font-semibold capitalize text-gray-600"
     >
-      <NavItem v-for="(icon, page) in navItems" :href="`#${page}`" :key="page">
+      <NavItem
+        @click="currentPage = page"
+        v-for="(icon, page) in navItems"
+        :href="`#${page}`"
+        :key="page"
+        :class="{ 'pointer-events-none text-purple-600': currentPage === page }"
+      >
         <component :is="icon" class="w-6" /> {{ page }}
       </NavItem>
     </ul>
