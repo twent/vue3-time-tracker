12c0549c8171c63a9516f4cea5888a4fee0578e0
twent <i@twent.ru>
2023-04-29 21:05:33 +0300

    ✔ Add BaseSelect, BaseButton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="rounded bg-gray-200 p-2 enabled:hover:bg-gray-300 dissabled:cursor-not-allowed disabled:opacity-50">
+        <slot></slot>
+    </button>
+</template>
